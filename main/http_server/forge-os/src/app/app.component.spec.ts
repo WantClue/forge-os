ad2fb4753fd764c7664c28a1319632ae5224c17d
WantClue <wantclue@users.noreply.github.com>
2026-04-19 23:08:05 +0200

    remove redundant title tests from AppComponent spec

--- main/http_server/forge-os/src/app/app.component.spec.ts
+++ main/http_server/forge-os/src/app/app.component.spec.ts
@@ -13,17 +13,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'forge-os'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('forge-os');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('forge-os app is running!');
-  });
 });
